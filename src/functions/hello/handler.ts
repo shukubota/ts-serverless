43a24c947de308de0830c5005c4d3f264d4194b0
shukubota <kubotashu0421@gmail.com>
2023-05-14 00:43:22 +0900

    update hello

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -5,8 +5,10 @@ import { middyfy } from '@libs/lambda';
 import schema from './schema';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  console.log({ event });
+
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+    message: `aHello ${event.body.name}, welcome to the exciting Serverless world!`,
     event,
   });
 };
