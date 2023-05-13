46a1cf7f2abdea24e323ef7e4c9ecf81a610eeef
shukubota <kubotashu0421@gmail.com>
2023-05-14 01:00:19 +0900

    invalid

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,13 +1,15 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
+import { t } from './testa';
 
 import schema from './schema';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   console.log({ event });
-  const a: number = "aaa";
+  const a: number = "aaaa";
   console.log({ a });
+  console.log({ t });
 
   return formatJSONResponse({
     message: `aHello ${event.body.name}, welcome to the exciting Serverless world!`,
